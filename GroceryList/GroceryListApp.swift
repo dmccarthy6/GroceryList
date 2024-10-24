379686ac11e69018bd691044d1cc878097f3accb
Dylan McCarthy <mccarthy.dylan@gmail.com>
2024-10-24 12:58:52 -0400

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Dylan  on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryListTests/GroceryListTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListTests.swift
+//  GroceryListTests
+//
+//  Created by Dylan  on 10/24/24.
+//
+
+import Testing
+@testable import GroceryList
+
+struct GroceryListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
